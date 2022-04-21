cb350198e71ef9f9b48d3a525e515250c0324b1b
skraggzila <jarnet6@gmail.com>
2022-04-21 07:53:17 +0300

    Lab 1

--- rpolab/app/src/main/java/com/example/rpolab/MainActivity.java
+++ rpolab/app/src/main/java/com/example/rpolab/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.rpolab;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
@@ -20,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,27 +34,23 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        String s_key = "key is a 16 char";
+        String original = new String("rpo-2022");
+
+        byte[] key = s_key.getBytes(StandardCharsets.UTF_8);
+
+        byte[] encrypted = encrypt(key, original.getBytes());
+        String decrypted = new String(decrypt(key, encrypted));
+
+        Log.d("rpolab_", "ORIGINAL: " + original + " / BYTES " + original.getBytes());
+        Log.d("rpolab_", "KEY: " + s_key + " / " + Arrays.toString(key) + " / LENGTH " + key.length);
+        Log.d("rpolab_", "ENCRYPTED: " + Arrays.toString(encrypted));
+        Log.d("rpolab_", "DECRYPTED: " + decrypted);
+
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
-
-        byte[] key =
-                stringToHex("0123456789ABCDEF0123456789ABCDE0");
-        byte[] enc = encrypt(key,
-                stringToHex("000000000000000102"));
-        byte[] dec = decrypt(key, enc);
-        String s = new String(Hex.encodeHex(dec)).toUpperCase();
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
-    }
-
-    public static byte[] stringToHex(String s) {
-        byte[] hex;
-        try {
-            hex = Hex.decodeHex(s.toCharArray());
-        } catch (DecoderException ex) {
-            hex = null;
-        }
-        return hex;
+//        TextView tv = binding.sampleText;
+//        tv.setText(stringFromJNI() + "\nencrypted " +
+//                Arrays.toString(encrypted)+"\ndecrypted " + decrypted);
     }
 
     /**
