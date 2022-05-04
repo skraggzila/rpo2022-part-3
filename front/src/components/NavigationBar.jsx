62c8fedc67cc072db926fc4367766c52622450bb
skraggzila <jarnet6@gmail.com>
2022-05-04 17:01:25 +0300

    Lab 9

--- front/src/components/NavigationBar.jsx
+++ front/src/components/NavigationBar.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate, Link } from 'react-router-dom';
+import Utils from "../utils/Utils";
 
 const NavigationBar = () => {
     let navigate = useNavigate();
+    let uname = Utils.getUserName();
+
+    const goHome = () => {
+        navigate("home", { replace: true });
+    }
+
+    const logout = () => {
+        Utils.removeUser();
+        goHome();
+    }
 
     return (
         <Navbar bg="light" expand="lg">
@@ -13,9 +24,18 @@ const NavigationBar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href={"\home"} >Home</Nav.Link>
-                    <Nav.Link onClick={() => {navigate("Home", { replace: true })}}>Another Home</Nav.Link>
+                    {/*<Nav.Link href={"\home"}>Home</Nav.Link>*/}
+                    <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                    <Nav.Link onClick={goHome}>Another Home</Nav.Link>
+                    <Nav.Link onClick={goHome}>Yet Another Home</Nav.Link>
                 </Nav>
+                <Navbar.Text>{uname}</Navbar.Text>
+                { uname &&
+                    <Nav.Link onClick={logout}><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Выход</Nav.Link>
+                }
+                { !uname &&
+                    <Nav.Link as={Link} to="/login"><FontAwesomeIcon icon={faUser} fixedWidth />{' '}Вход</Nav.Link>
+                }
             </Navbar.Collapse>
         </Navbar>
     );
